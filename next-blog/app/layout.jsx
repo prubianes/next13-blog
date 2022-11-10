2f1fc425be533a54677bf8ee90b759900723d628
Pablo Rubianes <pablorubianes@gmail.com>
2022-11-10 02:36:40 -0300

    Home page, blog posts missing

--- next-blog/app/layout.jsx
+++ next-blog/app/layout.jsx
@@ -1,14 +1,13 @@
 import './globals.css'
+import styles from './page.module.css'
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body>{children}</body>
+      <div className={styles.container}>
+        <main>{children}</main>
+      </div>
     </html>
   )
 }
--- next-blog/app/page.jsx
+++ next-blog/app/page.jsx
@@ -1,57 +1,30 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import profilePic from '../public/images/profile.jpg'
+
+const name = 'Pablo Rubianes';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js 13!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>app/page.tsx</code>
+    <>
+      <header className={styles.header}>
+            <Image
+              priority
+              src={profilePic}
+              className={styles.borderCircle}
+              height={144}
+              width={144}
+              alt={name}
+              />
+            <h1 className={styles.heading2Xl}>{name}</h1>
+      </header>
+      <section className={styles.headingMd}>
+        <p>A Fullstack dev from Uruguay</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://beta.nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js 13</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Explore the Next.js 13 playground.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates/next.js/app-directory?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
+      </section>
+    </>
   )
 }
